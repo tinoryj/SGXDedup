b805476aa782676a0866393f1b846af07d12aebc
tinoryj <tinoryj@gmail.com>
2020-08-17 12:35:49 +0800

    update for pow batch size test

--- test/enclaveBatch.sh
+++ test/enclaveBatch.sh
@@ -3,13 +3,12 @@
 # path of input file
 file='/home/tinoryj/TestData/2G.file'
 
-batchSizeSet=('1' '10' '100' '500' '1000' '1500' '2000' '2500' '3000' '3500' '4000')
+batchSizeSet=('1' '10' '100' '500' '1000' '2000'  '3000' '4000')
 
 for batchSize in ${batchSizeSet[@]}; do
 	cp ../test/config-${batchSize}.json ./config.json
-	for i in {1..1};do
+	for i in {1..10};do
     	echo ${batchSize}-${i} 
-		./client-sgx -s ${file}
 		./client-sgx -s ${file}
 		rm -rf .keyGenStore
 	done
