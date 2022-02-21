8a6553118f65667c31cde5d5c062f36259e1e49a
Yanjing Ren <renyanjing2018@icloud.com>
2022-02-21 14:29:46 +0800

    update script

--- Scripts/environmentInstall.sh
+++ Scripts/environmentInstall.sh
@@ -5,34 +5,91 @@
 if [ ! -d "Packages" ]; then
   mkdir Packages
 fi
-sudo apt install build-essential cmake wget libssl-dev libcurl4-openssl-dev libprotobuf-dev libboost-all-dev libleveldb-dev libsnappy-dev
+
+echo "Install system build prerequests"
+sudo apt install -y build-essential cmake wget libssl-dev libcurl4-openssl-dev libprotobuf-dev libboost-all-dev libleveldb-dev libsnappy-dev
 cd ./Packages/
+
 echo "Download SGX Driver"
-wget https://download.01.org/intel-sgx/sgx-linux/2.7/distro/ubuntu18.04-server/sgx_linux_x64_driver_2.6.0_4f5bb63.bin
+if [ ! -f "sgx_linux_x64_driver_1.41.bin" ]; then
+  wget https://download.01.org/intel-sgx/sgx-linux/2.15.1/distro/ubuntu20.04-server/sgx_linux_x64_driver_1.41.bin
+fi
+if [ ! -f "sgx_linux_x64_driver_2.11.0_2d2b795.bin" ]; then
+  wget https://download.01.org/intel-sgx/sgx-linux/2.15.1/distro/ubuntu20.04-server/sgx_linux_x64_driver_2.11.0_2d2b795.bin
+fi
+
 echo "Download SGX SDK"
-wget https://download.01.org/intel-sgx/sgx-linux/2.7/distro/ubuntu18.04-server/sgx_linux_x64_sdk_2.7.100.4.bin
-echo "Download SGX PSW"
-wget https://download.01.org/intel-sgx/sgx-linux/2.7/distro/ubuntu18.04-server/libsgx-enclave-common_2.7.100.4-bionic1_amd64.deb
-echo "Download SGX SSL"
-wget https://github.com/intel/intel-sgx-ssl/archive/refs/tags/lin_2.5_1.1.1d.zip
-echo "Download OpenSSL for SGX SSL"
-wget https://www.openssl.org/source/old/1.1.1/openssl-1.1.1d.tar.gz
-echo "Download SGX CMake"
-wget https://raw.githubusercontent.com/xzhangxa/SGX-CMake/master/cmake/FindSGX.cmake
-cp FindSGX.cmake /usr/share/cmake-3.10/Modules/
-chmod +x sgx_linux_x64_driver_2.6.0_4f5bb63.bin
-sudo ./sgx_linux_x64_driver_2.6.0_4f5bb63.bin
-sudo dpkg -i ./libsgx-enclave-common_2.7.100.4-bionic1_amd64.deb
-chmod +x sgx_linux_x64_sdk_2.7.100.4.bin
-sudo ./sgx_linux_x64_sdk_2.7.100.4.bin --prefix=/opt/intel
+if [ ! -f "sgx_linux_x64_sdk_2.15.101.1.bin" ]; then
+  wget https://download.01.org/intel-sgx/sgx-linux/2.15.1/distro/ubuntu20.04-server/sgx_linux_x64_sdk_2.15.101.1.bin
+fi
+
+echo "Install SGX Driver & SDK"
+targetPackageSet=('sgx_linux_x64_driver_1.41.bin' 'sgx_linux_x64_driver_2.11.0_2d2b795.bin' 'sgx_linux_x64_sdk_2.15.101.1.bin')
+for filename in ${targetPackageSet[@]}; do
+  if test -x $filename ; then
+    ls -l $filename
+  else
+    chmod +x $filename
+  fi
+done
+sudo ./sgx_linux_x64_driver_1.41.bin
+sudo ./sgx_linux_x64_driver_2.11.0_2d2b795.bin
+sudo ./sgx_linux_x64_sdk_2.15.101.1.bin --prefix=/opt/intel
 source /opt/intel/sgxsdk/environment
-unzip lin_2.5_1.1.1d.zip
-cp openssl-1.1.1d.tar.gz intel-sgx-ssl-lin_2.5_1.1.1d/openssl_source/
-cd intel-sgx-ssl-lin_2.5_1.1.1d/Linux/
+
+echo "Install SGX runtime environment"
+if [ ! -f "/etc/apt/sources.list.d/intel-sgx.list" ]; then
+  echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+fi
+wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add
+sudo apt install -y libsgx-epid libsgx-quote-ex libsgx-dcap-ql libsgx-enclave-common-dbgsym libsgx-dcap-ql-dbgsym libsgx-dcap-default-qpl-dbgsym libsgx-urts libsgx-launch libsgx-enclave-common-dev libsgx-uae-service
+sudo usermod -aG sgx_prv $USER
+sudo apt install -y libsgx-dcap-default-qpl libsgx-enclave-common-dev libsgx-dcap-ql-dev libsgx-dcap-default-qpl-dev
+sudo apt install -y ocaml ocamlbuild automake autoconf libtool python-is-python3 git perl libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper reprepro unzip
+git clone https://github.com/intel/linux-sgx.git
+if [ ! -d "linux-sgx" ]; then
+  echo "Error clone linux-sgx packages, please check github connection status, and run this scripts again"
+  exit
+else 
+  cd linux-sgx && make preparation
+  if [ "`ls -A external/toolset/ubuntu20.04/`" = "" ]; then
+    echo "Error build linux-sgx packages, please check github connection status or local compiler environment , and run this scripts again"
+    exit
+  else
+    sudo cp external/toolset/ubuntu20.04/* /usr/local/bin
+  fi
+fi
+
+echo "Download SGX SSL"
+if [ ! -f "lin_2.15.1_1.1.1l.zip" ]; then
+  wget https://github.com/intel/intel-sgx-ssl/archive/refs/tags/lin_2.15.1_1.1.1l.zip
+  if [ ! -f "lin_2.15.1_1.1.1l.zip" ]; then
+    echo "Error download sgx ssl package from github, please check github connection status, and run this scripts again"
+    exit
+  else
+    unzip lin_2.15.1_1.1.1l.zip
+  fi
+else
+  unzip lin_2.15.1_1.1.1l.zip
+fi
+
+echo "Download OpenSSL for SGX SSL"
+if [ ! -f "openssl-1.1.1l.tar.gz" ]; then
+  wget https://www.openssl.org/source/old/1.1.1/openssl-1.1.1l.tar.gz
+  cp openssl-1.1.1l.tar.gz intel-sgx-ssl-lin_2.15.1_1.1.1l/openssl_source/
+else
+  cp openssl-1.1.1l.tar.gz intel-sgx-ssl-lin_2.15.1_1.1.1l/openssl_source/
+fi
+
+echo "Install SGX SSL"
+cd intel-sgx-ssl-lin_2.15.1_1.1.1l/Linux/
 make all
 make test
 sudo make install
-cd /opt/intel/sgxssl/include
-sudo mv pthread.h sgxpthread.h
-sudo sed -i '415c #include \"sgxpthread.h\"' /opt/intel/sgxssl/include/openssl/crypto.h
-echo "Environment set done"
+
+echo `ls /dev/ | grep -q "^sgx$"`
+if [ $? -ne 0 ] ;then
+    echo "Some problems may occur, please check whether the SGX function is turned on in the BIOS, restart the device, and run the script again"
+else
+    echo "Environment set done"
+fi
